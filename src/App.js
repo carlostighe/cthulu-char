ddd2abe4152a33233cecf88ce90af026a1b3a6fe
Carlos Tighe <carlos.tighe@insight-centre.org>
2023-03-24 13:31:32 +0000

    login working but extract to service

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 
 import React, { useEffect, useState } from "react";
 
+import login from "./login2.svg";
+
 const gapi = window.gapi;
 const google = window.google;
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
@@ -12,13 +14,19 @@ const DISCOVERY_DOCS = [
 const SCOPES = "https://www.googleapis.com/auth/drive";
 function App() {
   const [tokenClient, setTokenClient] = useState(null);
-  const [gapiInited, setgapiInitiated] = useState(false);
-  const [gisInited, setgisInitiated] = useState(false);
+  const [gapiInited, setgapiInitited] = useState(false);
+  const [gisInited, setgisInitited] = useState(false);
+  const [accessToken, setAccessToken] = useState(null);
 
   const gapInit = async () => {
-    await gapi.client.init({}).then(function () {
-      setgapiInitiated(true);
-    });
+    await gapi.client
+      .init({
+        apiKey: API_KEY,
+        discoveryDocs: DISCOVERY_DOCS,
+      })
+      .then(function () {
+        setgapiInitited(true);
+      });
   };
   useEffect(() => {
     gapi.load("client", gapInit);
@@ -29,59 +37,62 @@ function App() {
         callback: "", // defined at request time
       })
     );
-    setgisInitiated(true);
+    setgisInitited(true);
   }, []);
 
+  const listFiles = async () => {
+    let response;
+    try {
+      response = await gapi.client.drive.files.list({
+        q: "name='character.json'",
+        fields: "files(id, name)",
+      });
+    } catch (err) {
+      console.log("err ", err.message);
+      return;
+    }
+    const characterData = await gapi.client.drive.files.get({
+      fileId: response.result.files[0].id,
+      alt: "media",
+    });
+    console.log("characterData ", characterData.body);
+  };
+
   const authorise = () => {
-    tokenClient.callback = (resp) => {
+    tokenClient.callback = async (resp) => {
       if (resp.error !== undefined) {
         throw resp;
       }
-      // GIS has automatically updated gapi.client with the newly issued access token.
-      console.log(
-        "gapi.client access token: " + JSON.stringify(gapi.client.getToken())
-      );
-      // make a call to get character.json file here
+      setAccessToken(gapi.client.getToken());
+      await listFiles();
     };
-
-    // Conditionally ask users to select the Google Account they'd like to use,
-    // and explicitly obtain their consent to fetch their Calendar.
-    // NOTE: To request an access token a user gesture is necessary.
-    if (gapi.client.getToken() === null) {
-      // Prompt the user to select a Google Account and asked for consent to share their data
-      // when establishing a new session.
-      tokenClient.requestAccessToken({ prompt: "consent" });
-    } else {
-      // Skip display of account chooser and consent dialog for an existing session.
-      tokenClient.requestAccessToken({ prompt: "" });
-    }
+    accessToken === null
+      ? tokenClient.requestAccessToken({ prompt: "consent" })
+      : tokenClient.requestAccessToken({ prompt: "" });
   };
 
   const revokeToken = () => {
-    console.log(gapi.client.getToken());
-    console.log(tokenClient);
-    if (tokenClient !== null) {
-      google.accounts.oauth2.revoke(tokenClient.access_token, () => {
-        console.log("Revoked: " + tokenClient.access_token);
-      });
+    if (accessToken !== null) {
+      google.accounts.oauth2.revoke(accessToken.access_token);
       gapi.client.setToken("");
-      setTokenClient(null);
+      setAccessToken(null);
     }
   };
 
   return (
     <div>
-      {gapiInited && gisInited ? (
-        <div className="row">
-          <button id="authorize_button" onClick={authorise}>
-            Authorize
-          </button>
-          <button id="signout_button" onClick={revokeToken}>
-            Sign Out
-          </button>
+      {!accessToken ? (
+        <div className="row login">
+          <img
+            src={login}
+            alt="cthulu like demon glyph"
+            onClick={authorise}
+          ></img>
         </div>
       ) : (
-        <h1>Issues....</h1>
+        <button id="signout_button" onClick={revokeToken}>
+          Sign Out
+        </button>
       )}
     </div>
   );
