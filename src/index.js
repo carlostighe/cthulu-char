15e4f00db9e284257638a509724404266da93b40
Carlos Tighe <carlos.tighe@insight-centre.org>
2023-06-22 16:07:38 +0100

    swiper

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import "./index.css";
 import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom/client";
+// import function to register Swiper custom elements
+import { register } from "swiper/element/bundle";
 import reportWebVitals from "./reportWebVitals";
-// import "./skeleton.css";
+
+// register Swiper custom elements
+register();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
