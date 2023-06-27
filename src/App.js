22edd9705fc04ff912b246ae7d410b785270ad67
carlos tighe <carlostighe@gmail.com>
2023-06-27 23:18:27 +0100

    twirly login

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import React, { useEffect, useState } from "react";
 import CharacterSheet from "./components/characters/CharacterSheet";
 import characterDataJson from "./data/character.json";
 import login from "./login2.svg";
+import {ReactComponent as CthuluLogo} from './login2.svg';
+
 
 function App() {
+  const [rotation, setRotation] = useState(0);
+
   const [accessToken, setAccessToken] = useState(null);
   const [fileId, setFIleId] = useState("");
   const [characterData, setCharacterData] = useState("");
@@ -15,15 +19,21 @@ function App() {
     setAccessToken(true);
   };
 
+  const handleClick =() => {
+    setRotation(rotation + 360)
+    setTimeout(() => authorise(), 1000);
+
+  }
   return (
     <div className="uk-height-viewport">
       {!accessToken ? (
         <div className="uk-height-match uk-position-center">
-          <img
-            src={login}
-            alt="cthulu like demon glyph"
-            onClick={authorise}
-          ></img>
+          <CthuluLogo 
+            style={{ 
+              transition: 'transform 1s',
+              transform: `rotate(${rotation}deg)` }}
+            onClick={handleClick}          
+          />
         </div>
       ) : (
         <div className="uk-height-viewport">
